6a8d41d48035a4f69a9b366946448c85a5d84a68
marco.vargas <mvargas@accuray.com>
2021-09-28 22:58:03 -0600

    Clase 02 - Trabajo en Clase

--- Lab01/main.tf
+++ Lab01/main.tf
@@ -20,6 +20,125 @@ resource "azurerm_resource_group" "patito" {
   name     = "iacPrueba"
   location = "centralus"
   tags = {
-    "Env" = "Dev"
+    "env" = "dev"
   }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "iacvnet"
+  location            = azurerm_resource_group.patito.location
+  resource_group_name = azurerm_resource_group.patito.name
+  address_space       = ["10.0.0.0/16"]
+  tags = {
+    "env" = "dev"
+  }
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "iacsubnet1"
+  resource_group_name  = azurerm_resource_group.patito.name
+  address_prefixes     = ["10.0.0.0/24"]
+  virtual_network_name = azurerm_virtual_network.vnet.name
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "iacnic"
+  location            = azurerm_resource_group.patito.location
+  resource_group_name = azurerm_resource_group.patito.name
+  ip_configuration {
+    name                          = "iacipconfig"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+  tags = {
+    "env" = "dev"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "iacvm1"
+  location              = azurerm_resource_group.patito.location
+  resource_group_name   = azurerm_resource_group.patito.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_D1_v2"
+
+  delete_data_disks_on_termination = "true"
+  delete_os_disk_on_termination    = "true"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "iacos"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "iacuacreativa01"
+    admin_username = "iacadmin"
+    admin_password = "Iacreativa01"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    "env" = "dev"
+  }
+}
+
+resource "azurerm_public_ip" "publicip" {
+  name                = "iacpublicip"
+  location            = azurerm_resource_group.patito.location
+  resource_group_name = azurerm_resource_group.patito.name
+  allocation_method   = "Static"
+  tags = {
+    "env" = "dev"
+  }
+}
+
+resource "azurerm_network_security_group" "iacsecgroup" {
+  name                = "iacSecGroup"
+  location            = azurerm_resource_group.patito.location
+  resource_group_name = azurerm_resource_group.patito.name
+  tags = {
+    "env" = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "rule01" {
+  name                        = "DenegarTodo"
+  network_security_group_name = azurerm_network_security_group.iacsecgroup.name
+  resource_group_name         = azurerm_resource_group.patito.name
+  priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
+
+resource "azurerm_network_security_rule" "rule02" {
+  name                        = "permitael80"
+  network_security_group_name = azurerm_network_security_group.iacsecgroup.name
+  resource_group_name         = azurerm_resource_group.patito.name
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
+
+resource "azurerm_network_interface_security_group_association" "sgassociation" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.iacsecgroup.id
 }
